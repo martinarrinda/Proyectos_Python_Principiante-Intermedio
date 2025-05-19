1e04bc82c3b448ffef931749d814d5e39f41718c
Martin Arrinda <martinarrinda@gmail.com>
2025-05-19 15:38:19 +0200

    Conversor de monedas finalizado

--- Juego_dado.py
+++ Juego_dado.py
@@ -1,8 +1,8 @@
 import random
 while True:
-    dado_jugador = random.randint(0,6)
+    dado_jugador = random.randint(1,6)
     print(f"Tu número: {dado_jugador}")
-    dado_maquina = random.randint(0,6)
+    dado_maquina = random.randint(1,6)
     print(f"Número de la máquina: {dado_maquina}")
     if dado_maquina < dado_jugador:
         print("¡Has ganado!")
